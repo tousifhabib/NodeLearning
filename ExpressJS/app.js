710aa81a05ac709c4fe855a0cf073111a9bfd993
Tousif Habib <thabib@mun.ca>
2021-05-28 01:01:29 -0230

    Finishing everything in express.js module

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,14 +1,21 @@
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); //Allows request to continue to next middleware in line
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    console.log('Another middleware');
-    res.send('<h1>Hello from express<h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
 });
 
-app.listen(3001);
+
+app.listen(3000);
