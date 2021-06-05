4251f48855dc19f8ccd290c7a172d5f82a368653
Tousif Habib <thabib@mun.ca>
2021-06-05 16:35:35 -0230

    Added HTML rendering through .pug files

--- Dynamic Content and Templating Engines/app.js
+++ Dynamic Content and Templating Engines/app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -14,7 +17,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
+    res.status(404).render('404');
 });
 
 
